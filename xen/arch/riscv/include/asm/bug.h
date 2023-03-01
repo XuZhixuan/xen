f4645ad4b5dd8321db0b538e57a5541557eb7d5e
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-03-01 12:20:48 +0200

    xen/riscv: introduce dummy <asm/bug.h>
    <xen/lib.h> will be used in the patch "xen/riscv: introduce
    decode_cause() stuff" and requires <asm/bug.h>
    
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/include/asm/bug.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2012 Regents of the University of California
+ * Copyright (C) 2021-2023 Vates
+ *
+ */
+#ifndef _ASM_RISCV_BUG_H
+#define _ASM_RISCV_BUG_H
+
+#endif /* _ASM_RISCV_BUG_H */
