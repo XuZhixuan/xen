603df1065bd6eabe5d4d192f0354c2db6d56804a
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-03-28 17:23:26 +0300

    xen/riscv: introduce <asm/setup.h>
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/include/asm/setup.h
@@ -0,0 +1,16 @@
+#ifndef __RISCV_SETUP_H_
+#define __RISCV_SETUP_H_
+
+#include <public/version.h>
+
+extern domid_t max_init_domid;
+
+#endif /* __RISCV_SETUP_H_ */
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
