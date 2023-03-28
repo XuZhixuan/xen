95277a50dfd1c56481b0ccc7a917655d763cb571
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-03-28 17:32:19 +0300

    xen/riscv: introduce <asm/mem_access.h>
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/include/asm/mem_access.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright 2019 (C) Alistair Francis <alistair.francis@wdc.com>
+ */
+
+#ifndef _ASM_RISCV_MEM_ACCESS_H
+#define _ASM_RISCV_MEM_ACCESS_H
+
+#endif /* _ASM_RISCV_MEM_ACCESS_H */
