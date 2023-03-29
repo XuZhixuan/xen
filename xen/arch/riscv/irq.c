cfaf4c90c83a74c8be299ce9724b8b662ce42522
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-03-29 19:09:06 +0300

    xen/riscv: introudce riscv/irq.c
    Add minimal functionality to build Xen common code.
    
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/irq.c
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* RISC-V Interrupt support */
+
+#include <xen/bug.h>
+#include <xen/sched.h>
+
+const unsigned int nr_irqs = NR_IRQS;
+
+hw_irq_controller no_irq_type = {
+};
+
+int arch_init_one_irq_desc(struct irq_desc *desc)
+{
+    assert_failed("need to be implemented");
+    return 0;
+}
+
+struct pirq *alloc_pirq_struct(struct domain *d)
+{
+    assert_failed("need to be implemented");
+    return NULL;
+}
+
+int pirq_guest_bind(struct vcpu *v, struct pirq *pirq, int will_share)
+{
+    assert_failed("need to be implemented");
+}
+
+void pirq_guest_unbind(struct domain *d, struct pirq *pirq)
+{
+    assert_failed("need to be implemented");
+}
+
+void pirq_set_affinity(struct domain *d, int pirq, const cpumask_t *mask)
+{
+    assert_failed("need to be implemented");
+}
+
+void smp_send_state_dump(unsigned int cpu)
+{
+    assert_failed("need to be implemented");
+}
+
+void arch_move_irqs(struct vcpu *v)
+{
+    /*
+     * TODO: there is no interrupt support in RISC-V for time being so
+     *       we can only print a message instead of assert
+     */
+    printk("%s: need to be implemented", __func__);
+}
+
