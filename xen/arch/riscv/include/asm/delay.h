c1371f10f91de2cfcd097f0e26f72eabd05b2dcc
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-03-29 15:41:34 +0300

    xen/riscv: introduce <asm/delay.h>
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/include/asm/delay.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2009 Chen Liqin <liqin.chen@sunplusct.com>
+ * Copyright (C) 2016 Regents of the University of California
+ */
+
+#ifndef _ASM_RISCV_DELAY_H
+#define _ASM_RISCV_DELAY_H
+
+#define udelay udelay
+extern void udelay(unsigned long usecs);
+
+#endif /* _ASM_RISCV_DELAY_H */
