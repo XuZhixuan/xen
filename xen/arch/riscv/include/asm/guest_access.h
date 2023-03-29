94dcbac6aec2e3237951f91694096c0f55452b7c
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-03-29 15:43:54 +0300

    xen/riscv: introduce <asm/guest_access.h>
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/include/asm/guest_access.h
@@ -0,0 +1,24 @@
+#ifndef __ASM_RISCV_GUEST_ACCESS_H__
+#define __ASM_RISCV_GUEST_ACCESS_H__
+
+/* common/symbol.c complain with this header: it should moved there */
+#include <xen/errno.h>
+
+unsigned long raw_copy_to_guest(void *to, const void *from, unsigned len);
+unsigned long raw_copy_from_guest(void *to, const void *from, unsigned len);
+
+#define __raw_copy_to_guest raw_copy_to_guest
+#define __raw_copy_from_guest raw_copy_from_guest
+
+#define guest_handle_okay(hnd, nr) (1)
+#define guest_handle_subrange_okay(hnd, first, last) (1)
+
+#endif /* __ASM_RISCV_GUEST_ACCESS_H__ */
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
