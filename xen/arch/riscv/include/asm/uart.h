400bef9c36bf3f6ff522bf30c0e9b24d0d29f428
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-04-26 12:43:08 +0300

    xen/riscv: introduce uart_init
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/include/asm/uart.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * RISC-V Platform-Level Interrupt Controller support
+ */
+
+#ifndef __ASM_RISCV_UART_H__
+#define __ASM_RISCV_UART_H__
+
+void uart_init(void);
+
+#endif
