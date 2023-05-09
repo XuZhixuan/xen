1ada984df1233f3ae20ffef13326013015fb5eb2
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-05-09 18:56:29 +0300

    xen/riscv: implement riscv_vcpu_unpriv_read()
    The function reads machine word from guest memory.
    
    The code was introduced by Bobby but part related to
    trap handler of riscv_unpriv_trap() was re-written to
    C instead of assembler
    
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

--- xen/arch/riscv/guestcopy.c
+++ xen/arch/riscv/guestcopy.c
@@ -3,15 +3,20 @@
 #include <xen/errno.h>
 #include <xen/sched.h>
 
+#include <asm/csr.h>
+#include <asm/guest_access.h>
+#include <asm/system.h>
+#include <asm/traps.h>
+
 unsigned long raw_copy_to_guest(void *to, const void *from, unsigned len)
 {
-	WARN();
+    WARN();
     return -ENOSYS;
 }
 
 unsigned long raw_copy_from_guest(void *to, const void __user *from, unsigned len)
 {
-	WARN();
+    WARN();
     return -ENOSYS;
 }
 
@@ -60,6 +65,127 @@ unsigned long copy_to_guest_phys(struct domain *d,
     }
 
     return 0;
+}
+
+__attribute__((interrupt("supervisor")))
+static void riscv_unpriv_trap(void)
+{
+    /* The trap details will be saved at address pointed by 'A0' register */
+    register struct riscv_trap *trap asm("a0");
+
+    /*
+     * We assume that faulting unpriv load/store instruction is 4-byte long
+     * and blindly increment SEPC by 4.
+     */
+    trap->sepc = csr_read(CSR_SEPC) + 4;
+    csr_write(CSR_SEPC, trap->sepc);
+
+    trap->scause = csr_read(CSR_SCAUSE);
+    trap->stval = csr_read(CSR_STVAL);
+    trap->htval = csr_read(CSR_HTVAL);
+    trap->htinst = csr_read(CSR_HTINST);
+}
+
+/**
+ * riscv_vcpu_unpriv_read -- Read machine word from Guest memory
+ *
+ * @vcpu: The VCPU pointer
+ * @read_insn: Flag representing whether we are reading instruction
+ * @guest_addr: Guest address to read
+ * @trap: Output pointer to trap details
+ */
+unsigned long riscv_vcpu_unpriv_read(struct vcpu *vcpu,
+                     bool read_insn,
+                     unsigned long guest_addr,
+                     struct riscv_trap *trap)
+{
+    register unsigned long taddr asm("a0") = (unsigned long)trap;
+    register unsigned long ttmp asm("a1");
+    register unsigned long val asm("t0");
+    register unsigned long tmp asm("t1");
+    register unsigned long addr asm("t2") = guest_addr;
+    unsigned long flags;
+    unsigned long old_stvec, old_hstatus;
+
+    local_irq_save(flags);
+
+    old_hstatus = csr_swap(CSR_HSTATUS, vcpu->arch.hstatus);
+    old_stvec = csr_swap(CSR_STVEC, (unsigned long)&riscv_unpriv_trap);
+
+    if (read_insn) {
+        /*
+         * HLVX.HU instruction
+         * 0110010 00011 rs1 100 rd 1110011
+         */
+        asm volatile ("\n"
+            ".option push\n"
+            ".option norvc\n"
+            "add %[ttmp], %[taddr], 0\n"
+            /*
+             * HLVX.HU %[val], (%[addr])
+             * HLVX.HU t0, (t2)
+             * 0110010 00011 00111 100 00101 1110011
+             */
+            ".word 0x6433c2f3\n"
+            "andi %[tmp], %[val], 3\n"
+            "addi %[tmp], %[tmp], -3\n"
+            "bne %[tmp], zero, 2f\n"
+            "addi %[addr], %[addr], 2\n"
+            /*
+             * HLVX.HU %[tmp], (%[addr])
+             * HLVX.HU t1, (t2)
+             * 0110010 00011 00111 100 00110 1110011
+             */
+            ".word 0x6433c373\n"
+            "sll %[tmp], %[tmp], 16\n"
+            "add %[val], %[val], %[tmp]\n"
+            "2:\n"
+            ".option pop"
+        : [val] "=&r" (val), [tmp] "=&r" (tmp),
+          [taddr] "+&r" (taddr), [ttmp] "+&r" (ttmp),
+          [addr] "+&r" (addr) : : "memory");
+
+        if (trap->scause == CAUSE_LOAD_PAGE_FAULT)
+            trap->scause = CAUSE_FETCH_PAGE_FAULT;
+    } else {
+        /*
+         * HLV.D instruction
+         * 0110110 00000 rs1 100 rd 1110011
+         *
+         * HLV.W instruction
+         * 0110100 00000 rs1 100 rd 1110011
+         */
+        asm volatile ("\n"
+            ".option push\n"
+            ".option norvc\n"
+            "add %[ttmp], %[taddr], 0\n"
+#ifdef CONFIG_64BIT
+            /*
+             * HLV.D %[val], (%[addr])
+             * HLV.D t0, (t2)
+             * 0110110 00000 00111 100 00101 1110011
+             */
+            ".word 0x6c03c2f3\n"
+#else
+            /*
+             * HLV.W %[val], (%[addr])
+             * HLV.W t0, (t2)
+             * 0110100 00000 00111 100 00101 1110011
+             */
+            ".word 0x6803c2f3\n"
+#endif
+            ".option pop"
+        : [val] "=&r" (val),
+          [taddr] "+&r" (taddr), [ttmp] "+&r" (ttmp)
+        : [addr] "r" (addr) : "memory");
+    }
+
+    csr_write(CSR_HSTATUS, old_hstatus);
+    csr_write(CSR_STVEC, old_stvec);
+
+    local_irq_restore(flags);
+
+    return val;
 }
 
 /*
