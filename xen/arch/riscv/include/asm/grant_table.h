a8487dd9c2644d13ce74f3109cdc60f788b07b00
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-05-10 17:15:14 +0300

    xen/riscv: introduce gnttab_dom0_frames()
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

--- xen/arch/riscv/include/asm/grant_table.h
+++ xen/arch/riscv/include/asm/grant_table.h
@@ -1,6 +1,16 @@
 #ifndef __ASM_GRANT_TABLE_H__
 #define __ASM_GRANT_TABLE_H__
 
+/*
+ * The region used by Xen on the memory will never be mapped in DOM0
+ * memory layout. Therefore it can be used for the grant table.
+ *
+ * Only use the text section as it's always present and will contain
+ * enough space for a large grant table
+ */
+#define gnttab_dom0_frames()                                             \
+    min_t(unsigned int, opt_max_grant_frames, PFN_DOWN(_etext - _stext))
+
 #endif /* __ASM_GRANT_TABLE_H__ */
 /*
  * Local variables:
