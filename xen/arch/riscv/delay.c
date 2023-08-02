4e27c830e66acbf95788eeb4e9a4bcf2cddbbae3
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-08-02 16:53:20 +0300

    xen/riscv: implement udelay
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

--- xen/arch/riscv/delay.c
+++ xen/arch/riscv/delay.c
@@ -1,8 +1,12 @@
-#include <xen/bug.h>
+#include <xen/time.h>
+
+#include <asm/processor.h>
 
 void udelay(unsigned long usecs)
 {
-    assert_failed("need to be implemented");
+    s_time_t deadline = get_s_time() + 1000 * (s_time_t) usecs;
+
+    while ( get_s_time() - deadline < 0 )
+        cpu_relax();
 }
 EXPORT_SYMBOL(udelay);
-
