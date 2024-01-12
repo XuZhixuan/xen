eca348a752ccaf03f0ed0148389519a9496c2e5a
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2024-01-12 13:56:27 +0200

    xen/riscv: introduce generic interrupt controller abstraction
    The abstraction provides operation for interrupt controllers.
    
    The code based on Arm's gic.{h,c} and was provided by
    Romain Caritey <romain.caritey@microchip.com>.
    
    The following changes were done on top:
    * Move gic.{h,c} changes to separate patch
    * add SPDX
    * fix code styles.
    * remove GIC_APLIC/GIC_PLIC cases as the drivers aren't introduced
      for time being.
    * add gic_init_secondary_cpu().
    * add a check that variable 'v' was properlyu allocated in gic_alloc_vgic().
    * add a check that git_ops->init isn't NULL during gic_init().
    
    Signed-off-by: Romain Caritey <romain.caritey@microchip.com>
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/gic.c
@@ -0,0 +1,150 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * xen/arch/riscv/gic.c
+ *
+ * Based on Arm xen/arch/arm/gic.c
+ *
+ * Generic Interrupt Controler
+ *
+ * (c) 2024 Microchip Technology Inc.
+ */
+
+#include <xen/sched.h>
+#include <asm/setup.h>
+#include <xen/types.h>
+
+#include <asm/gic.h>
+
+static const struct gic_hw_operations *gic_ops = NULL;
+
+void* gic_get_private(void)
+{
+    if ( gic_ops->get_private )
+        return gic_ops->get_private();
+
+    return NULL;
+}
+
+/* Set up the per-CPU parts of the GIC for a secondary CPU */
+void gic_init_secondary_cpu(void)
+{
+    if ( gic_ops->secondary_init )
+        gic_ops->secondary_init();
+}
+
+int gic_make_hwdom_dt_node(struct domain *d,
+                           const struct dt_device_node *gic,
+                           void *fdt)
+{
+    ASSERT(gic == dt_interrupt_controller);
+
+    if ( gic_ops && gic_ops->make_dom_dt_node )
+        return gic_ops->make_dom_dt_node(d, gic, fdt);
+
+    return 0;
+}
+
+int gic_make_domu_dt_node(struct domain *d, void *fdt)
+{
+    if ( gic_ops && gic_ops->make_dom_dt_node )
+        return gic_ops->make_dom_dt_node(d, gic_ops->info->node, fdt);
+
+    return 0;
+}
+
+int gic_iomem_deny_access(struct domain *d)
+{
+    if ( gic_ops && gic_ops->iomem_deny_access )
+        return gic_ops->iomem_deny_access(d);
+
+    return 0;
+}
+
+int gic_register_domain(const struct domain *d)
+{
+    if ( gic_ops && gic_ops->register_domain )
+        return gic_ops->register_domain(d);
+
+    return 0;
+}
+
+int gic_unregister_domain(const struct domain *d)
+{
+    if ( gic_ops && gic_ops->unregister_domain )
+        return gic_ops->unregister_domain(d);
+
+    return 0;
+}
+
+void gic_ops_register(const struct gic_hw_operations *ops)
+{
+    gic_ops = ops;
+}
+
+static void __init gic_dt_preinit(void)
+{
+    int rc;
+    struct dt_device_node *node;
+    uint8_t num_gics = 0;
+
+    dt_for_each_device_node( dt_host, node )
+    {
+        if ( !dt_get_property(node, "interrupt-controller", NULL) )
+            continue;
+
+        if ( !dt_get_parent(node) )
+            continue;        
+
+        rc = device_init(node, DEVICE_GIC, NULL);
+        if ( !rc )
+        {
+            /* NOTE: Only one GIC is supported */
+            num_gics = 1;
+            break;
+        }
+    }
+    if ( !num_gics )
+        panic("Unable to find a compatible GIC in the device tree\n");
+
+    /* Set the GIC as the primary interrupt controller */
+    dt_interrupt_controller = node;
+    dt_device_set_used_by(node, DOMID_XEN);
+}
+
+void __init gic_init(void)
+{
+    if ( gic_ops->init && gic_ops->init() )
+        panic("Failed to initialize the GIC drivers\n");
+}
+
+void __init gic_preinit(void)
+{
+    gic_dt_preinit();
+}
+
+struct vgic* gic_alloc_vgic(struct vcpu *vcpu)
+{
+    struct vgic *v;
+
+    switch ( gic_ops->info->hw_version )
+    {
+    default:
+        printk(XENLOG_WARNING "need to add allocation vgic?\n");
+        return NULL;
+    }
+
+    if ( v )
+        v->info = gic_ops->info;
+
+    /* phandle will be updated during the dom0/domU creation */
+    return v;
+}
+
+void gic_free_vgic(struct vgic *v)
+{
+    switch ( gic_ops->info->hw_version )
+    {
+    default:
+        panic("Unsupported free of gic\n"); 
+    }
+}
---
+++ xen/arch/riscv/include/asm/gic.h
@@ -0,0 +1,113 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * xen/arch/riscv/gic.h
+ *
+ * Based on xen/arch/arm/gic.h
+ *
+ * Generic Interrupt Controler
+ *
+ * (c) 2024 Microchip Technology Inc.
+ *
+ */
+
+#ifndef __ASM_RISCV_GIC_H__
+#define __ASM_RISCV_GIC_H__
+
+#include <xen/irq.h>
+#include <xen/init.h>
+
+enum gic_version {
+    GIC_INVALID = 0,    /* the default until explicitly set up */
+    GIC_PLIC,
+    GIC_APLIC,
+};
+
+struct gic_info {
+    /* GIC version */
+    enum gic_version hw_version;
+    /* Pointer to the device tree node representing the interrupt controller */
+    const struct dt_device_node *node;
+    /* interrupt cell size of GIC */
+    int interrupt_cell_size;
+    /* private data pointer of the interrupt controller */
+    void *private;
+};
+
+struct gic_hw_operations {
+    /* Hold GIC HW information */
+    const struct gic_info *info;
+    /* Initialize the GIC and the boot CPU */
+    int (*init)(void);
+    /* Save GIC registers */
+    void (*save_state)(struct vcpu *);
+    /* Restore GIC registers */
+    void (*restore_state)(const struct vcpu *);
+    /* Dump GIC LR register information */
+    void (*dump_state)(const struct vcpu *);
+    /* register a domain */
+    int (*register_domain)(const struct domain *d);
+    /* unregister domain */
+    int (*unregister_domain)(const struct domain *d);
+    /* hw_irq_controller to enable/disable/eoi host irq */
+    hw_irq_controller *gic_host_irq_type;
+    /* hw_irq_controller to enable/disable/eoi guest irq */
+    hw_irq_controller *gic_guest_irq_type;
+    /* End of Interrupt */
+    void (*eoi_irq)(struct irq_desc *irqd);
+    /* Deactivate/reduce priority of irq */
+    void (*deactivate_irq)(struct irq_desc *irqd);
+    /* Read IRQ id and Ack */
+    unsigned int (*read_irq)(void);
+    /* Force the active state of an IRQ by accessing the distributor */
+    void (*set_active_state)(struct irq_desc *irqd, bool state);
+    /* Force the pending state of an IRQ by accessing the distributor */
+    void (*set_pending_state)(struct irq_desc *irqd, bool state);
+    /* Set IRQ type */
+    void (*set_irq_type)(struct irq_desc *desc, unsigned int type);
+    /* Set IRQ priority */
+    void (*set_irq_priority)(struct irq_desc *desc, unsigned int priority);
+    /* Disable CPU physical and virtual interfaces */
+    void (*disable_interface)(void);
+    /* Query the pending state of an interrupt at the distributor level. */
+    bool (*read_pending_state)(struct irq_desc *irqd);
+    /* Secondary CPU init */
+    int (*secondary_init)(void);
+    /* Create GIC node for domain */
+    int (*make_dom_dt_node)(struct domain *d,
+                              const struct dt_device_node *gic, void *fdt);
+    /* Map extra GIC MMIO, irqs and other hw stuffs to the hardware domain. */
+    int (*map_hwdom_extra_mappings)(struct domain *d);
+    /* Deny access to GIC regions */
+    int (*iomem_deny_access)(struct domain *d);
+    /* get private section */
+    void * (*get_private)(void);
+};
+
+struct vgic {
+    const struct gic_info *info;
+    int (*emulate_load)(struct vcpu *vcpu, unsigned long addr, uint32_t *out);
+    int (*emulate_store)(struct vcpu *vcpu, unsigned long addr, uint32_t in);
+    int (*is_access)(struct vcpu *vcpu, unsigned long address);
+};
+
+void gic_preinit(void);
+void gic_init(void);
+
+void gic_ops_register(const struct gic_hw_operations *ops);
+int gic_register_domain(const struct domain *d);
+int gic_unregister_domain(const struct domain *d);
+int gic_make_hwdom_dt_node(struct domain *d,
+                           const struct dt_device_node *gic,
+                           void *fdt);
+int gic_make_domu_dt_node(struct domain *d, void *fdt);
+
+struct vgic* gic_alloc_vgic(struct vcpu *vcpu);
+void gic_free_vgic(struct vgic *v);
+
+void* gic_get_private(void);
+
+int gic_iomem_deny_access(struct domain *d);
+
+void gic_init_secondary_cpu(void);
+
+#endif /* __ASM_RISCV_GIC_H__ */
