78b82df9f968ced12d647655953b6bcaf46e3d36
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2024-01-22 13:35:20 +0200

    xen/riscv: fix include guard name for vsbi uart
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

---
+++ xen/arch/riscv/include/asm/vsbi.h
@@ -0,0 +1,18 @@
+/*
+ * xen/arch/riscv/insclude/asm/vsbi.h
+ *
+ *  Handle sbi calls from guest
+ *
+ * Copyright (c) 2024 Microchip.
+ *
+ */
+
+#ifndef __ASM_RISCV_VSBI_H__
+#define __ASM_RISCV_VSBI_H__
+
+struct vcpu;
+struct regs;
+
+void vsbi_handle_ecall(struct vcpu *vcpu, struct cpu_user_regs *regs);
+
+#endif
